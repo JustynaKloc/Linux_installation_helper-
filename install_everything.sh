27cbc4fc8b7e550f1dd51abf77ec8f61b5e27196
Justyna <jkloc96@gmail.com>
2020-09-15 10:45:06 +0200

    init

---
+++ install_everything.sh
@@ -0,0 +1,37 @@
+#instalacja i ustawienie plasmy 
+
+
+#instalacja pythona
+sudo apt-get update
+sudo apt-get install python3.6
+sudo apt-get update
+sudo apt-get -y install python3-pip
+
+#instalacja pakietów podstawonych
+
+pip3 install Keras
+pip3 install numpy
+pip3 install pandas
+pip3 install matplotlib
+pip3 install cv2
+
+sudo apt-get install docker 
+
+#instalacja przeglądarki
+
+sudo apt install chromium-browser
+
+#instalacja spotify
+
+sudo snap install spotify
+
+#instalacja VScode
+
+sudo apt update
+sudo apt install software-properties-common apt-transport-https wget
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update
+sudo apt install code
+
+#instalacja Javy :<<
